a0f76810db132713ef2b9d41ed598adbfd381c0b
Olivia <oereeve@gmail.com>
2016-02-09 15:48:06 -0500

    more styling

--- backend/app/models/super.rb
+++ backend/app/models/super.rb
@@ -1,5 +1,5 @@
 class Super < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :category, presence: true, inclusion: { in: ["Hero", "Anti-Hero", "Villain"]}
+  validates :category, presence: true, inclusion: { in: ["Hero", "hero", "Anti-Hero", "anti-hero", "Villain", "villain"]}
   validates :description, presence: true
 end
